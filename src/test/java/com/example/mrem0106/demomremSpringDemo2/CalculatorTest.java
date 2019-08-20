737104d442d7da7dda32a0c40cf93858ef6eca1b
fpoh <fabianpohlink@gmx.de>
2019-08-20 14:28:39 +0200

    update

--- src/test/java/com/example/mrem0106/demomremSpringDemo2/CalculatorTest.java
+++ src/test/java/com/example/mrem0106/demomremSpringDemo2/CalculatorTest.java
@@ -1,13 +1,24 @@
 package com.example.mrem0106.demomremSpringDemo2;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @Before
+    public void setUp () {
+        calculator = new Calculator();
+    }
 
     @Test
     public void calculate() {
+        Assert.assertEquals(3, calculator.calculate(1, 2, Operator.plus), 0);
+        Assert.assertEquals(0.25, calculator.calculate(1, 4, Operator.geteilt), 0);
     }
 
     @Test
