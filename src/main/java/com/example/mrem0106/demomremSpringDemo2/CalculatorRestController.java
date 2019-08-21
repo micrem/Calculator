b62121c60df5aa619cf160c2bee0921835938661
mrem <michael.rempel@mail.schwarz>
2019-08-21 14:20:52 +0200

    included BL

--- src/main/java/com/example/mrem0106/demomremSpringDemo2/CalculatorRestController.java
+++ src/main/java/com/example/mrem0106/demomremSpringDemo2/CalculatorRestController.java
@@ -1,11 +1,13 @@
 package com.example.mrem0106.demomremSpringDemo2;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
+@CrossOrigin
 public class CalculatorRestController {
     @GetMapping(path = "/calcrest")
     public CalculatorDataString calculate(@RequestParam String a, @RequestParam String b, @RequestParam String operant) {
